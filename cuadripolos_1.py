c7a82d42564a7180395eaca80a3237099f166c14
Abel Lamas <abel.synyster@gmail.com>
2022-06-08 23:43:09 -0300

    multiply matrix working

--- cuadripolos_1.py
+++ cuadripolos_1.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+def change_section():
+  return np.array([[1,0],[0,1]])
+
 def tube(f,c,z,r,l):
   s = np.pi*r**2
   k = (2*np.pi*f)/c
@@ -23,28 +26,61 @@ def transmission_loss(A,B,C,D,s,z):
 
 
 
+#frequency range to obtain values
+f_range = np.arange(20, 20000, 1)
 
-f_range = np.arange(20, 40, 1)
 
 # ejecutar el tubo para cada frecuencia y guardar los datos en un array
-tube_values = []
-side_b_values = []
-matrix_multiply = []
-transmission_values = []
+# execute the tube for each frequency and save the data on an array
+
+c=343 #sound propagation velocity
+z=410 #characteristic impedance of the medium
+r_tube=0.025 #radius tube in
+r_chamber=0.061 #radius expansion chamber
+l_in=0.15 #lenght inlet tube
+l_chamber=0.0596 #lenght expansion chamber
+l_conection=0.15 #lenght tube conection between mufflers
+l_sb=0.30 #lenght side branch
+l_out=0.15 #lenght outlet tube
+
+T1_values = [] #matrix tube lenght l_in
+T2_values = [] #matrix change section
+T3_values = [] #matrix tube lenght l_chamber
+T4_values = [] #matrix change section
+T5_values = [] #matrix tube lenght l_conection
+T6_values = [] #matrix side_branch
+T7_values = [] #matrix tube lenght l_out
+matrix_multiply = [] #contains all the resultants matrix
+transmission_values = [] #contains all the TL values for each matrix
+
+# tube_values = []
+# side_b_values = []
+# for f in f_range:
+#   tube_values.append(tube(f=f,c=343,z=410,r=0.025,l=0.15))
+#   side_b_values.append(side_branch(f=f,c=343,z=410,r_b=0.025,l=0.3))
+
 for f in f_range:
-  tube_values.append(tube(f=f,c=343,z=410,r=0.025,l=0.15))
-  side_b_values.append(side_branch(f=f,c=343,z=410,r_b=0.025,l=0.3))
+  T1_values.append(tube(f,c,z,r_tube,l_in))
+  # T2_values.append(change_section())
+  T3_values.append(tube(f,c,z,r_chamber,l_chamber))
+  # T4_values.append(change_section())
+  T5_values.append(tube(f,c,z,r_tube,l_conection))
+  T6_values.append(side_branch(f,c,z,r_tube,l_sb))
+  T7_values.append(tube(f,c,z,r_tube,l_out)) 
 
 
 for i in range(len(f_range)):
-  matrix_multiply.append(tube_values[i]@side_b_values[i])
+  # T_result = T1_values[i]@T2_values[i]@T3_values[i]@T4_values[i]@T5_values[i]@T6_values[i]@T7_values[i] #consider all the elements
+  # T_result = T1_values[i]@T3_values[i]@T5_values[i]@T6_values[i]@T7_values[i] #delete the matrix of change section
+  T_result = T3_values[i]@T5_values[i]@T6_values[i] #delete the matrix of inlet tube, change section and outlet tube
+  matrix_multiply.append(T_result)
 
-for i in range(len(f_range)):
-  A,B,C,D=matrix_multiply[i][0,0], matrix_multiply[i][0,1], matrix_multiply[i][1,0], matrix_multiply[i][1,1]
+for j in range(len(f_range)):
+  A,B,C,D=matrix_multiply[j][0,0], matrix_multiply[j][0,1], matrix_multiply[j][1,0], matrix_multiply[j][1,1]
   transmission_values.append(transmission_loss(A,B,C,D,0.025,410))
 
-print(transmission_values)
 
-# plt.plot(f_range,transmission_loss(f_range,r=0.025, z=410))
-# plt.show()
+
+plt.plot(f_range,transmission_values)
+plt.show()
 
